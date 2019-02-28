27be4dffe6d6add3266dfcae93f0ee466b0cd637
CSHwang <cshwang4869@outlook.com>
2019-02-28 10:02:20 +0800

    Accepted.

--- Luogu/Luogu2410.cpp
+++ Luogu/Luogu2410.cpp
@@ -1,5 +1,6 @@
 #include <queue>
 #include <math.h>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <algorithm>
@@ -8,35 +9,23 @@
 #define DEBUG printf("Passing [%s] in Line %d\n" , __FUNCTION__ , __LINE__) ;
 
 const db eps = 1e-7 ;
-const int MAX_N = 100 + 10 , MAX_P = 200 + 10 , MAX_M = 2e4 + 10 , INF = 0x3f3f3f3f ;
+const int MAX_N = 100 + 10 , MAX_P = 200 + 10 , INF = 0x3f3f3f3f ;
 
-struct Link {
-    int num , cap ; db val ;
-    Link *next , *reg ;
-}list[MAX_M << 1] ;
+struct data {int num , cap , reg ; db val ;} ;
 
 std::queue<int> que ;
+std::vector<data> eg[MAX_P] ;
 
 bool vis[MAX_P] ;
 db dis[MAX_P] , dt[MAX_N] ;
-Link *head[MAX_P] , *id[MAX_P] ;
-int T , tot , pre[MAX_P] , flow[MAX_P] ;
+int T , tot , pre[MAX_P] , flow[MAX_P] , id[MAX_P] ;
 int n , m , cur , r[MAX_N] , c[MAX_N] , P[MAX_N][MAX_N] , ans[MAX_N][MAX_N] ;
 
 void ins(int x , int y , int f , db v) {
-    list[cur].num = y ;
-    list[cur].cap = f ;
-    list[cur].val = v ;
-    list[cur].next = head[x] ;
-    list[cur].reg = &list[cur^1] ;
-    head[x] = &list[cur++] ;
+	int p1 = eg[x].size() , p2 = eg[y].size() ;
 
-    list[cur].num = x ;
-    list[cur].cap = 0 ;
-    list[cur].val = -v ;
-    list[cur].next = head[y] ;
-    list[cur].reg = &list[cur^1] ;
-    head[y] = &list[cur++] ;
+	eg[x].push_back((data){y , f , p2 , v}) ;
+	eg[y].push_back((data){x , 0 , p1 , -v}) ;
 }
 
 bool spfa() {
@@ -46,15 +35,15 @@ bool spfa() {
     dis[0] = flow[T] = 0 ; vis[0] = 1 ; flow[0] = INF ;
 
     for (; !que.empty() ;) {
-        int x = que.front() ;
+        int x = que.front() , siz = eg[x].size() ;
         que.pop() ; vis[x] = 0 ;
 
-        for (Link *h = head[x] ; h ; h = h->next) {
-            int nx = h->num ;
-            if (!h->cap || dis[nx] - dis[x] - h->val < eps) continue ;
+        for (int i = 0 ; i < siz ; ++i) {
+            int nx = eg[x][i].num ;
+            if (!eg[x][i].cap || dis[nx] - dis[x] - eg[x][i].val < eps) continue ;
 
-            dis[nx] = dis[x] + h->val ;
-            flow[nx] = std::min(flow[x] , h->cap) ; pre[nx] = x ; id[nx] = h ;
+            dis[nx] = dis[x] + eg[x][i].val ;
+            flow[nx] = std::min(flow[x] , eg[x][i].cap) ; pre[nx] = x ; id[nx] = i ;
 
             if (!vis[nx]) que.push(nx) , vis[nx] = 1 ;
         }
@@ -66,8 +55,10 @@ bool spfa() {
 void work() {
     for (; spfa() ;) {
         int f = flow[T] ;
-        for (int x = T ; x ; x = pre[x])
-            id[x]->cap -= f , id[x]->reg->cap += f ;
+        for (int x = T ; x ;) {
+        	int p = id[x] , y = x ; x = pre[x] ;
+        	eg[x][p].cap -= f ; eg[y][eg[x][p].reg].cap += f ;
+        }
     }
 }
 
@@ -88,7 +79,7 @@ int main() {
 
     ///
 
-    dt[0] = MAX_M ;
+    dt[0] = INF ;
     for (int i = 1 ; i < 100 ; ++i) dt[i] = -log(i) ;
     for (int i = 1 ; i <= n ; ++i) r[i] = ++tot ;
     for (int i = 1 ; i <= m ; ++i) c[i] = ++tot ;
@@ -110,12 +101,14 @@ int main() {
         	ins(r[i] , c[j] , 1 , dt[P[i][j]]) ;
 
     work() ;
-    for (int x = 1 ; x <= n ; ++x)
-        for (Link *h = head[x] ; h ; h = h->next) {
-            int nx = h->num ;
+    for (int x = 1 ; x <= n ; ++x) {
+    	int siz = eg[x].size() ;
+        for (int i = 0 ; i < siz ; ++i) {
+            int nx = eg[x][i].num ;
             if (!nx) continue ;
-            else if (!h->cap) ans[x][nx - n] = 1 ;
+            else if (!eg[x][i].cap) ans[x][nx - n] = 1 ;
         }
+    }
 
     for (int i = 1 ; i <= n ; ++i , putchar('\n'))
         for (int j = 1 ; j <= m ; ++j) putchar('0' + ans[i][j]) ;
